868f7cb99b3ea130c4b2630eab779c2b0234624d
Aditya8094 <adityatyagi1224@gmail.com>
2025-10-04 15:34:17 +0530

    Added Reports.jsx

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -5,7 +5,7 @@ export default function Reports() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Reports</h1>
-      <p>View charts and reports of company expenses.</p>
+      <p className="font-mg bg-zinc-600 text-xl ">View charts and reports of company expenses.</p>
     </div>
   );
 }
